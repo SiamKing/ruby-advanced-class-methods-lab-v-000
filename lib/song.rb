de9fe4d8369b6562e0cb353ed748f2dc40312141
SiamKing <thesiameseelectric@gmail.com>
2016-03-18 09:37:45 -0700

    destroy all please

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(song_string)
+    song = self.new
+    song.name = song_string
+    song
+  end
+
+  def self.create_by_name(song_string)
+    song = self.new
+    song.name = song_string
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(song_string)
+    @@all.detect {|song| song.name == song_string}
+  end
+
+  def self.find_or_create_by_name(song_string)
+    self.find_by_name(song_string)
+    self.create_by_name(song_string)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(artist_and_song)
+    artist_and_song.slice!(".mp3")
+    art_arr = artist_and_song.split(" - ")
+    song = self.new
+    song.name = art_arr[1]
+    song.artist_name = art_arr[0]
+    song
+  end
+
+  def self.create_from_filename(artist_and_song)
+    artist_and_song.slice!(".mp3")
+    art_arr = artist_and_song.split(" - ")
+    song = self.new
+    song.name = art_arr[1]
+    song.artist_name = art_arr[0]
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
